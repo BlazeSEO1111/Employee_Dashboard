5a2fade38dc5bccd0a892301e3349fb01119acc9
BroCodeW <anvukog@gmail.com>
2024-07-25 10:41:53 +0700

    fix load fail api

--- src/api-client/auth/index.ts
+++ src/api-client/auth/index.ts
@@ -1,129 +1,129 @@
 import ApiClientBase from "../ApiClientBase";
-import { AccountDetailResponse, LoginParamsType, LoginResponseType, RegisterParamsType } from "../types/AuthType";
+import {AccountDetailResponse, LoginParamsType, LoginResponseType, RegisterParamsType} from "../types/AuthType";
 
 class ApiAuth extends ApiClientBase {
-  constructor() {
-    super();
-    this.instance.defaults.headers["Content-Type"] = "application/json";
-  }
+    constructor() {
+        super();
+        this.instance.defaults.headers["Content-Type"] = "application/json";
+    }
 
-  /**
-   * Login
-   */
-  public async login(params: LoginParamsType): Promise<LoginResponseType | any> {
-    const res = await this.instance.post("/api/v1/auth/sign-in", params);
-    return res;
-  }
+    /**
+     * Login
+     */
+    public async login(params: LoginParamsType): Promise<LoginResponseType | any> {
+        const res = await this.instance.post("/api/v1/auth/sign-in", params);
+        return res;
+    }
 
-  /**
-   * sign up
-   */
-  public async signUp(params: RegisterParamsType): Promise<any> {
-    const res = await this.instance.post("/api/v1/auth/sign-up", params, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    return res;
-  }
+    /**
+     * sign up
+     */
+    public async signUp(params: RegisterParamsType): Promise<any> {
+        const res = await this.instance.post("/api/v1/auth/sign-up", params, {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        return res;
+    }
 
-  /**
-   * confirm email123123@123123@123123@
-   */
-  public async confirmEmail(token: string, access_token: string): Promise<any> {
-    const res = await this.instance.post(
-      "/api/app/account-extend/confirm-email",
-      {
-        token: token.replaceAll(" ", "+"),
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: "Bearer " + access_token,
-        },
-      }
-    );
-    return res.data;
-  }
+    /**
+     * confirm email123123@123123@123123@
+     */
+    public async confirmEmail(token: string, access_token: string): Promise<any> {
+        const res = await this.instance.post(
+            "/api/app/account-extend/confirm-email",
+            {
+                token: token.replaceAll(" ", "+"),
+            },
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: "Bearer " + access_token,
+                },
+            }
+        );
+        return res.data;
+    }
 
-  public async verifyEmail(access_token: string): Promise<any> {
-    const res = await this.instance.post(
-      "/api/app/account-extend/verify-email",
-      {},
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: "Bearer " + access_token,
-        },
-      }
-    );
-    return res.data;
-  }
+    public async verifyEmail(access_token: string): Promise<any> {
+        const res = await this.instance.post(
+            "/api/app/account-extend/verify-email",
+            {},
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: "Bearer " + access_token,
+                },
+            }
+        );
+        return res.data;
+    }
 
-  public async canCancel(access_token: string): Promise<any> {
-    const res = await this.instance.post(
-      "/api/app/payment/can-cancel",
-      {},
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: "Bearer " + access_token,
-        },
-      }
-    );
-    return res.data;
-  }
+    public async canCancel(access_token: string): Promise<any> {
+        const res = await this.instance.post(
+            "/api/app/payment/can-cancel",
+            {},
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: "Bearer " + access_token,
+                },
+            }
+        );
+        return res.data;
+    }
 
-  public async getAccountExtendDetails(accessToken: string, userId: string): Promise<AccountDetailResponse> {
-    const res = await this.instance.get(`/api/v1/users/${userId}`, {
-      headers: {
-        Authorization: "Bearer " + accessToken,
-      },
-    });
-    return res.data;
-  }
+    public async getAccountExtendDetails(accessToken: string, userId: string): Promise<AccountDetailResponse> {
+        const res = await this.instance.get(`/api/v1/users/get-user-by-id/${userId}`, {
+            headers: {
+                Authorization: "Bearer " + accessToken,
+            },
+        });
+        return res.data;
+    }
 
-  public async changePassword(access_token: string, currentPassword: string, newPassword: string): Promise<any> {
-    const res = await this.instance.post(
-      `/api/app/account-extend/change-password?currentPassword=${currentPassword}&newPassword=${newPassword}`,
-      {},
-      {
-        headers: {
-          Authorization: "Bearer " + access_token,
-        },
-      }
-    );
-    return res.data;
-  }
+    public async changePassword(access_token: string, currentPassword: string, newPassword: string): Promise<any> {
+        const res = await this.instance.post(
+            `/api/app/account-extend/change-password?currentPassword=${currentPassword}&newPassword=${newPassword}`,
+            {},
+            {
+                headers: {
+                    Authorization: "Bearer " + access_token,
+                },
+            }
+        );
+        return res.data;
+    }
 
-  public async resetPassword(email: string): Promise<any> {
-    const res = await this.instance.post(`/api/app/account-extend/reset-password?email=${email}`, {});
-    return res.data;
-  }
+    public async resetPassword(email: string): Promise<any> {
+        const res = await this.instance.post(`/api/app/account-extend/reset-password?email=${email}`, {});
+        return res.data;
+    }
 
-  public async confirmResetPassword(
-    email: string,
-    token: string,
-    password: string,
-    confirmPassword: string
-  ): Promise<any> {
-    const tokenFormat = token.replaceAll(" ", "+");
-    const res = await this.instance.post(
-      `/api/app/account-extend/confirm-reset-password`,
-      {
-        email,
-        token: tokenFormat,
-        password,
-        confirmPassword,
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    return res.data;
-  }
+    public async confirmResetPassword(
+        email: string,
+        token: string,
+        password: string,
+        confirmPassword: string
+    ): Promise<any> {
+        const tokenFormat = token.replaceAll(" ", "+");
+        const res = await this.instance.post(
+            `/api/app/account-extend/confirm-reset-password`,
+            {
+                email,
+                token: tokenFormat,
+                password,
+                confirmPassword,
+            },
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }
+        );
+        return res.data;
+    }
 }
 
 export default ApiAuth;
--- src/app/[locale]/client/profile-employee/page.tsx
+++ src/app/[locale]/client/profile-employee/page.tsx
@@ -1,86 +1,95 @@
 "use client"
 import Header from '@/components/Header'
 import AppLayout from '@/components/Layout/AppLayout'
-import React, { useContext, useEffect, useState } from 'react'
-import { Tabs } from 'antd';
+import React, {useContext, useState} from 'react'
+import {Tabs} from 'antd';
 import Notification from "./Notification/index"
 import MainInfor from './MainInfor';
-import { informationApi, notificationApi, paymentApi } from '@/api-client';
-import { AuthContext } from '@/context/useAuthContext';
-import { useQuery } from '@tanstack/react-query';
-import { useRouter } from 'next/router';
-import { toast } from 'react-toastify';
-import { ItemType } from '../recharge/page';
+import {informationApi, notificationApi} from '@/api-client';
+import {AuthContext} from '@/context/useAuthContext';
+import {useQuery} from '@tanstack/react-query';
+
 const ProfileEmployee = () => {
 
-  const [statusProfile, setStatusProfile] = useState("active")
-  const { handleLogOut, authState, accountExtendDetail, getAccountExtendDetails } = useContext(AuthContext);
+    const [statusProfile, setStatusProfile] = useState("active")
+    const {handleLogOut, authState, accountExtendDetail, getAccountExtendDetails} = useContext(AuthContext);
+    const {isPending, error, data} = useQuery({
+        queryKey: ["getPaymentHistory", authState?.accessToken],
+        queryFn: async () => {
+            if (authState?.userId) {
+                return await informationApi.getInformPublisher(authState?.accessToken ?? "", authState.userId);
+            } else {
+                return null; // Hoặc một giá trị mặc định khác nếu cần
+            }
+        },
+        enabled: !!authState?.userId, // Chỉ bật query nếu userId tồn tại
+    });
+    console.log("authState", authState)
+    console.log("authState.userId", authState?.userId)
+    console.log("datadatadata", data)
 
-  const { isPending, error, data } = useQuery<any>({
-    queryKey: ["getPaymentHistory", authState?.accessToken],
-    queryFn: async () => await informationApi.getInformPublisher(authState?.accessToken ?? "", authState?.userId ?? ""),
-  });
+    const {isLoading: isLoadingNotification, error: errorNotification, data: dataNotification} = useQuery<any>({
+        queryKey: ["getDataNotification", authState?.accessToken],
+        queryFn: async () => await notificationApi.getNotification(authState?.accessToken ?? ""),
+        enabled: !!authState?.userId,
+    });
 
-  const { isLoading: isLoadingNotification, error: errorNotification, data: dataNotification } = useQuery<any>({
-    queryKey: ["getDataNotification", authState?.accessToken],
-    queryFn: async () => await notificationApi.getNotification(authState?.accessToken ?? ""),
-  });
+    const items = [
+        {
+            key: '1',
+            label: 'Thông tin',
+            children: <MainInfor information={data}/>,
+        },
+        {
+            key: '2',
+            label: 'Thông báo',
+            children: <Notification dataNotification={dataNotification}/>,
+        },
+    ];
 
-  const items = [
-    {
-      key: '1',
-      label: 'Thông tin',
-      children: <MainInfor information={data} />,
-    },
-    {
-      key: '2',
-      label: 'Thông báo',
-      children: <Notification dataNotification={dataNotification} />,
-    },
-  ];
-
-  const renderStatusProfile = (value: string) => {
-    switch (value) {
-      case "active":
-        return (
-          <span className='text-green-600 font-semibold'>ACTIVE</span>
-        )
-      case "inactive":
-        return (
-          <span className='text-red-500 font-semibold'>INACTIVE</span>
-        )
-      default:
-        break;
+    const renderStatusProfile = (value: string) => {
+        switch (value) {
+            case "active":
+                return (
+                    <span className='text-green-600 font-semibold'>ACTIVE</span>
+                )
+            case "inactive":
+                return (
+                    <span className='text-red-500 font-semibold'>INACTIVE</span>
+                )
+            default:
+                break;
+        }
     }
-  }
 
-  if (!authState?.accessToken) return
-  <>
-    <AppLayout>
-      <div>Vui lòng đăng nhập </div>
-    </AppLayout>
-  </>
+    if (!authState?.accessToken) return <>
+        <AppLayout>
+            <div>Vui lòng đăng nhập</div>
+        </AppLayout>
+    </>
 
-  return (
+    return (
 
-    <AppLayout>
-      <div className="w-full  h-screen flex flex-col">
-        <div className="p-6">
-          <Header title={data} />
-          <div><span className='text-blue-600 font-semibold'>Trạng thái tài khoản</span> : {renderStatusProfile(statusProfile)}</div>
-          <div className='flex flex-col gap-3 mt-2'>
-            <p className='text-blue-600 font-semibold'>Quyền tài khoản:</p>
-            <p className='text-green-600 font-semibold'>Sale</p>
-          </div>
-          <div className="h-[1px] bg-black  bg-opacity-20 my-4 max-lg:hidden" >
-            <Tabs defaultActiveKey="1" items={items} />
-          </div>
-        </div>
-      </div>
+        <AppLayout>
+            <div className="w-full  h-screen flex flex-col">
+                <div className="p-6">
+                    <Header title={data}/>
+                    <div><span
+                        className='text-blue-600 font-semibold'>Trạng thái tài khoản</span> : {renderStatusProfile(statusProfile)}
+                    </div>
+                    <div className='flex flex-col gap-3 mt-2'>
+                        <p className='text-blue-600 font-semibold'>Quyền tài khoản:</p>
+                        <p className='text-green-600 font-semibold'>Sale</p>
+                    </div>
+                    <div className="h-[1px] bg-black  bg-opacity-20 my-4 max-lg:hidden">
+                        <Tabs defaultActiveKey="1" items={items}/>
+                    </div>
+                </div>
+            </div>
 
-    </AppLayout >
+        </AppLayout>
 
-  )
+    )
 }
 
 export default ProfileEmployee
